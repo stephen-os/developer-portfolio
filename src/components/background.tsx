97accfcdde3aaef76ab16d4fc38324010275b5a0
stephen-os <ImStephen.1sw@gmail.com>
2025-06-14 00:53:19 -0400

    fix: changed background to be neutral

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Background({ children }: { children?: React.ReactNode }) {
     return (
-        <main className="relative w-full min-h-screen bg-stone-900 text-white overflow-hidden">
+        <main className="relative w-full min-h-screen bg-neutral-900 text-white overflow-hidden">
             <div className="relative z-10 flex items-center justify-center h-full">
                 {children}
             </div>
